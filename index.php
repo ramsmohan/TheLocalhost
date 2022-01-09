f6bdd7ef346c3b9edcef772dd533935fe63bc5f7
Ram Mohan Sharma <rms.rammohansharma@gmail.com>
2022-01-09 21:06:59 +0530

    Index php added

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>TheLocalhost</title>
+	<style type="text/css">
+		body{ background: rgb(2,0,36);
+background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(71,93,153,1) 0%, rgba(121,9,80,1) 0%, rgba(56,28,110,1) 44%, rgba(0,0,0,1) 100%);
+		 color: rgba(255,255,255,1); font-family: monospace; 
+		 margin: 0 auto;
+		 padding: 10% 20%;
+		}
+		table, tr, td, th{border: 1px dashed #000; padding: 5px; min-width: 200px; text-align: center;}
+
+	</style>
+</head>
+<body>
+	<h1>TheLocalhost</h1>
+	<table>
+		<tr>
+			<th>Project</th>
+			<th>Link</th>
+		</tr>
+		<tr>
+			<td>ProjectName</td>
+			<td>Link</td>
+		</tr>
+	</table> 
+
+</body>
+</html>
+
+
+<?php
+	
+?>
