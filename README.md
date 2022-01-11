4be289142bfd9075724bb83e5e18d8e85ff72d7a
Ram Mohan Sharma <rms.rammohansharma@gmail.com>
2022-01-11 23:54:07 +0530

    Added Files List Code

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # TheLocalhost
 A cool and stylish Localhost Homepage for XAMPP, WAMP or LAMP stack. You can keep this in any directory to show its contents in a beautiful manner. Keep Enjoying, Keep Suggesting.
 
-# Steps to use after fresh XAMPP / WAMP / LAMP Stack installation
+## Steps to use after fresh XAMPP / WAMP / LAMP Stack installation
 
  1. Rename old index.php under your route directory of XAMPP/Wamp/Lamp stack installation.
  2. Copy the index.php from TheLocalhost
  3. Paste in the route directory of your route directory.
- 4. Cheers.
+ 4. **Cheers**
 
--- index.php
+++ index.php
@@ -3,6 +3,36 @@
 <head>
 	<meta charset="utf-8">
 	<title>TheLocalhost</title>
+
+</head>
+<body>
+	<h1>TheLocalhost</h1>
+	<table>
+		<!-- <tr>
+			<th>Project</th>
+			<th>Link</th>
+		</tr> -->
+		<tr><td colspan=2><input type="text" id="search_project" placeholder="Search Project Here"></td></tr>
+		<?php
+			$myDirs = scandir( __DIR__ );
+			foreach ($myDirs as $dir) 
+			{
+				if( ! is_file( $dir ) )
+				{
+					if(( $dir == '.' )|| ($dir == '..') )
+						echo ''; //'<tr><td colspan=2>Parent</td></tr>';
+					else
+						echo '<tr>
+								<td>'.strtoupper( $dir ).'</td>
+								<td><a href="../../'.strtolower( $dir ).'">Open Project</a></td>
+							</tr>'; ?>
+		<?php
+				}
+			}
+		?>
+	</table> 
+
+</body>
 	<style type="text/css">
 		body{ background: rgb(2,0,36);
 background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(71,93,153,1) 0%, rgba(121,9,80,1) 0%, rgba(56,28,110,1) 44%, rgba(0,0,0,1) 100%);
@@ -11,26 +41,10 @@ background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(71,93,153,1) 0%, rgb
 		 padding: 10% 20%;
 		}
 		table, tr, td, th{border: 1px dashed #000; padding: 5px; min-width: 200px; text-align: center;}
-
+		a{ color: antiquewhite;  }
+		input[type=text]{ width: 80%; background: rgba( 255,255,255,0.4 ); padding:5px 5px; border-radius: 4px; color: #fff; margin:5px; border: 1px solid #fff }
+		::-webkit-input-placeholder { color: antiquewhite; }
+		:-ms-input-placeholder { color: antiquewhite; }
+		::placeholder { color: antiquewhite; }
 	</style>
-</head>
-<body>
-	<h1>TheLocalhost</h1>
-	<table>
-		<tr>
-			<th>Project</th>
-			<th>Link</th>
-		</tr>
-		<tr>
-			<td>ProjectName</td>
-			<td>Link</td>
-		</tr>
-	</table> 
-
-</body>
 </html>
-
-
-<?php
-	
-?>
